c86c7f98b043d2df2f9c36f6dcc04843acc8e79e
JohnBiruk <birukovie@gmail.com>
2022-01-19 02:07:06 +0300

    version 2.0

--- body/index.py
+++ body/index.py
@@ -6,14 +6,35 @@ def encode(str):
     result = ""
     for key, freq in itertools.groupby(str):
         result += f"{len(list(freq))}" + key
+
+    lst = re.findall(r"(\d+.)", result)
+    lst.append("end")
+
+    result = ""; count = 0; clipboard = ""
+
+    for i in range(len(lst)-1):
+        if lst[i][:-1] == "1":
+            clipboard += lst[i][-1]
+            count += 1
+            if lst[i+1][:-1] != "1" or lst[i] == lst[-1]:
+                result += f"-{count}" + clipboard
+        else:
+            result += lst[i]
+            clipboard = ""
+            count = 0
     return result
 
 
 def decode(str):
     result = ""
-    lst = re.findall(r"(\d+.)", str)
+    lst = re.findall(r"(-?\d+[^0123456789-]+)", str)
     for i in lst:
-        result += int(i[:-1]) * i[-1]
+        if i[0] == "-":
+            for j in i[1:]:
+                if j.isalpha():
+                    result += j
+        else:
+            result += int(i[:-1]) * i[-1]
     return result
 
 
